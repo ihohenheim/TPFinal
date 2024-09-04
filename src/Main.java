b895cd0fb45d59def2c699accc20d25a15b81074
Shino <hohenheim66@gmail.com>
2024-09-03 23:32:49 -0400

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+/*Trabajo final  - Fundamentos de Programación 
+ * >>>>> Grupo conformado por:
+ * >>>>>> Aldo javier reyes Mella
+ * >>>>>> Daniel Iturry
+ * >>>>> Profesor:
+ * >>>>>> Cristian Cappo
+ * >>>>> Maestria en Ingenieria Electronica
+ * >>>>> Facultad Politecnica  - UNA
+ * >>>>>>>>>>> 2024 
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        MainMenu menu = new MainMenu();
+        menu.mostrarMenu();
+    }
+}
